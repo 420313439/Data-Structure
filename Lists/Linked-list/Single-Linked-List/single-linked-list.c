2760b4e33f7306d02497f922e10517eb39cd0f40
Peiwen Pan <420313439@qq.com>
2020-10-29 22:20:15 +0800

    add more func

--- Lists/Linked-list/Single-Linked-List/single-linked-list.c
+++ Lists/Linked-list/Single-Linked-List/single-linked-list.c
@@ -1,5 +1,5 @@
 // 单链表
-// gcc single-linked-node.c -g -Wall
+// gcc single-linked-node.c -g -Wall -O3 -o a.out
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +9,7 @@ struct Node {
     struct Node *next;
 };
 
-/***** 函数声明 *****/
+/***** 函数声明开始 *****/
 // 初始化头结点
 extern struct Node *init(int val);
 
@@ -17,10 +17,16 @@ extern struct Node *init(int val);
 extern struct Node *append(struct Node *head, int val);
 
 // 弹出链表末尾节点
-extern int pop(struct Node *head);
+extern struct Node *pop(struct Node *head);
 
 // 寻找节点
 extern struct Node *search(struct Node *head, int val);
+
+// 插入节点
+extern struct Node *insert(struct Node *head, int target_val, int val);
+
+// 删除节点
+extern struct Node *delete(struct Node *head, int val);
 
 // 打印链表
 extern void print_node(struct Node *head);
@@ -74,59 +80,116 @@ struct Node *append(struct Node *head, int val)
 /* 寻找节点 */
 struct Node *search(struct Node *head, int val)
 {
-    if (head == NULL)
-        puts("Empty Node");
+    struct Node *node = head;
 
-    while (head != NULL && val != head->val)
-        head = head->next;
+    if (node == NULL) {
+        puts("search(): Empty Node");
+        return NULL;
+    }
 
-    if (head == NULL)
-        puts("value not found");
-    else if (val == head->val)
-        puts("value found!");
-    
-    return head;
+    while (node != NULL && val != node->val)
+        node = node->next;
+    if (val == node->val)
+        return node;
+    else
+        return NULL;
 }
 
 
 /* 弹出链表末尾节点 */
-int pop(struct Node *head)
+struct Node *pop(struct Node *head)
 {
     //struct Node *head = *phead;
-    struct Node *pre_head;
+    struct Node *node = head;
+    struct Node *pre_head = NULL;
 
-    if (head == NULL)
-        puts("Empty Node");
+    if (node == NULL)
+        puts("pop(): Empty List");
 
-    while (head->next != NULL) {
-        pre_head = head;
-        head = head->next;
+    while (node->next != NULL) {
+        pre_head = node;
+        node = node->next;
     }
 
-    int val = head->val;
     pre_head->next = NULL;
-    free(head);
+    free(node);
 
-    return val;
+    return head;
 }
 
 
+/* 把val插入到值为target_val的节点之后 */
+struct Node *insert(struct Node *head, int target_val, int val)
+{
+    struct Node *target_node = search(head, target_val);
+    struct Node *new_node = init(val);
+
+    if (target_node == NULL) {
+        printf("insert(): No Such Node with %d", target_val);
+        return NULL;
+    }    
+
+    new_node->next = target_node->next;
+    target_node->next = new_node;
+
+    return head;
+}
+
+
+/* 删除值为val的节点 */
+struct Node *delete(struct Node *head, int val)
+{
+    struct Node *node = head;
+    struct Node *pre_node = NULL;
+    struct Node *target_node = search(head, val);
+
+    if (node == NULL) {
+        puts("delete(): Empty list");
+        return NULL;
+    }
+
+    // first node eq..
+    if (node == target_node) {
+        head = node->next;
+        free(node);
+        return head;
+    }
+
+    // 2nd after...
+    while (node != target_node && node != NULL) {
+        pre_node = node;
+        node = node->next;
+    }
+
+    if (node == target_node) {
+        pre_node->next = node->next;
+        free(node);
+    }
+
+    return head;
+}
 
 /* main函数 */
 int main(void)
 {
     struct Node *head = init(0);
 
-    head = append(head, 1);
-    head = append(head, 2);
-    head = append(head, 3);
-
+    append(head, 1);
+    append(head, 2);
+    append(head, 3);
     print_node(head);
-    int val = pop(head);
-    printf("val = %d\n", val);
+    
+    pop(head);
     print_node(head);
 
-    printf("%d\n", search(head, 2)->val);
+    insert(head, 1, 4);
+    print_node(head);
+
+    insert(head, 0, 5);
+    print_node(head);
+
+    head = delete(head, 0);
+    print_node(head);
 
     return 0;
 }
